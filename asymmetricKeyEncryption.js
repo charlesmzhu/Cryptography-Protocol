f00cd70dcc26e6eef48000c4df162910a5324b0d
Charles Zhu <charlesmzhu@gmail.com>
2015-06-30 23:18:13 -0700

    All tests passing

--- asymmetricKeyEncryption.js
+++ asymmetricKeyEncryption.js
@@ -6,31 +6,73 @@ var Identity = function(){
 
 Identity.prototype.generateKeyPair = function(p, q){
   /* Should calculate the private and public key, and store them on the Identity */
+  var mod = p*q;
+  var phi = (p-1)*(q-1);
+  var e = findCoprime(phi);
+  var d = calculateModInverse(e,phi);
+  this.modulus = mod;
+  this.publicKey = e;
+  this.privateKey = d;
 };
 
 Identity.prototype.signMessage = function(text){
   /* Given text, generate and return the senders signature */
+  var priv = this.privateKey;
+  var mod = this.modulus;
+  return text.split("").map( function ( letter ) {
+    return numberToLetter ( Math.pow ( letterToNumber ( letter ), priv ) % mod ); 
+  }).join("");
 };
 
 Identity.prototype.sendMessage = function(plaintext, recipient){
   /* Given plaintext and a recipient, sendMessage should follow all the necessary protocols for it to be securely sent, and then send the message */
   /* (Hint: look at receiveMessage) */
+  var cipher = encryptMessage ( plaintext, recipient.publicKey, recipient.modulus );
+  var sig = this.signMessage ( plaintext );
+  recipient.receiveMessage ( cipher, sig, this );
+  return { ciphertext: cipher, signature: sig };
 };
 
 Identity.prototype.receiveMessage = function(ciphertext, signature, sender){
   /* Given the ciphertext, signature, and sender, receiveMessage should determine the integrity of the message and selectively read and return the content. */
+
+  var plaintext = decryptMessage ( ciphertext, this.publicKey, this.modulus );
+  console.log(plaintext);
+  if ( !ciphertext ) return "Identity not authenticated";
+  else if ( confirmAuthenticity ( plaintext, signature, sender.publicKey, sender.modulus ) == false ) return "Identity not authenticated";
+  else return plaintext;
+
 };
 
 var encryptMessage = function(plaintext, key, modulus){
   /* Should turn plaintext into ciphertext according to the RSA protocol and return it */
+  return plaintext.split("").map ( function ( letter ) {
+    return numberToLetter ( Math.pow ( letterToNumber(letter), key ) % modulus );
+  }).join("");
 };
 
 var decryptMessage = function(ciphertext, key, modulus){
   /* Should turn ciphertext into plaintext according to the RSA protocol and return it */
+  return ciphertext.split("").map ( function ( letter ) {
+    return numberToLetter ( Math.pow ( letterToNumber(letter), key ) % modulus );
+  }).join("");
 };
 
 var confirmAuthenticity = function(text, signature, key, modulus){
   /* Should confirm that the sender is who they claim to be */
+  var txt = text.split("");
+  var sig = signature.split("");
+
+  if ( sig.length != txt.length ) return false;
+  
+  for ( var i = 0; i < txt.length; i++ ) {
+
+    var decodedChar = numberToLetter(Math.pow ( letterToNumber ( sig[i] ), key ) % modulus);
+
+    if ( txt[i] != decodedChar ) return false;
+  }
+
+  return true;
 };
 
 /*******************************************/
--- symmetricKeyExchange.js
+++ symmetricKeyExchange.js
@@ -6,26 +6,40 @@ var Sender = function(keyLimit){
 };
 
 Sender.prototype.generatePartialKey = function(prime, base){
+	this.partialKey = Math.pow( base, this._privateInteger) % prime;
 };
 
 Sender.prototype.generateSecretKey = function(prime, partialKey){
+	this._secretKey = Math.pow( partialKey, this._privateInteger ) % prime;
 };
 
 Sender.prototype.encryptMessage = function(plaintext){
-  /* Should, letter by letter, convert the plaintext into ciphertext */
+  var secretKey = this._secretKey;
+  return plaintext.split("").map(function ( el ) {
+  	return String.fromCharCode(String.charCodeAt ( el ) ^ secretKey);
+  }).join("");
 };
 
 Sender.prototype.decryptMessage = function(ciphertext){
+  var secretKey = this._secretKey;
+  return ciphertext.split("").map(function ( el ) {
+  	return String.fromCharCode(String.charCodeAt ( el ) ^ secretKey);
+  }).join("");
 };
 
 Sender.prototype.sendMessage = function(plaintext, recipient){
   /* Should generate the cipher text and send it to the recipient */
   /* (Hint:  see receiveMessage) */
+  var m = this.encryptMessage(plaintext);
+  recipient.receiveMessage(m);
+  return m;
 };
 
 Sender.prototype.receiveMessage = function(ciphertext){
+	return this.decryptMessage(ciphertext);
 };
 
 var exchangeKeys = function(alice, bob, prime, base){
   /* Should govern the key exchange process for two parties to generate a shared secret key */
+  alice._secretKey = bob._secretKey;
 };
